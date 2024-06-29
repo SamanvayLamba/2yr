1b2b4bb3f16bdf46972a64cf8569abbfc35bb034
Samanvay Lamba <lamsam@amazon.com>
2024-06-29 12:49:46 +0200

    Initial commit

---
+++ src/components/header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+    return (
+        <header className="header">
+            <h1>Happy 2-Year Anniversary!</h1>
+            <p>Welcome to our special anniversary page</p>
+        </header>
+    );
+};
+
+export default Header;
